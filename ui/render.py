0318b30fb2989741f06b09f1a4ed70ed8f395f0e
eduardor0cha <c.eduardo_rn@outlook.com>
2026-06-19 22:57:46 -0300

    fix: use CTkImage in render to fix HighDPI scaling warning

--- ui/render.py
+++ ui/render.py
@@ -2,7 +2,7 @@ import customtkinter as ctk
 import matplotlib.pyplot as plt
 import numpy as np
 
-from PIL import Image, ImageTk
+from PIL import Image
 from PIL.Image import Image as IMG
 from numpy.typing import NDArray
 from numpy import uint8
@@ -20,7 +20,7 @@ def array_to_pil(array: NDArray[uint8]) -> IMG:
 
 def fit_photoimage(
     array: NDArray[uint8], max_width: int, max_height: int
-) -> ImageTk.PhotoImage:
+) -> ctk.CTkImage:
     img: IMG = array_to_pil(array)
     width, height = img.size
 
@@ -32,7 +32,7 @@ def fit_photoimage(
                 Image.LANCZOS,
             )
 
-    return ImageTk.PhotoImage(img)
+    return ctk.CTkImage(light_image=img, dark_image=img, size=img.size)
 
 
 def render_image(frame: ctk.CTkFrame, array: NDArray[uint8]) -> None:
